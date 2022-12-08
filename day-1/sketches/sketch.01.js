81e8b7c9cb48cce2261c48761091899deffe8dd3
as00r <as00r.dev@gmail.com>
2022-12-09 02:03:22 +0530

    day1 - LEARN: canvas-sketch API

--- day-1/sketches/sketch.01.js
+++ day-1/sketches/sketch.01.js
@@ -1,14 +1,39 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+	dimensions: [1048, 1048],
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
-  };
+	return ({ context, width, height }) => {
+		context.fillStyle = "#32213A";
+		context.fillRect(0, 0, width, height);
+		const w = width * 0.05;
+		const h = height * 0.05;
+		const gap = width * 0.008;
+		const initialX = width * 0.21;
+		const initialY = height * 0.21;
+		const offset = width * 0.01;
+		let x, y;
+
+		for (let i = 0; i < 10; i++) {
+			for (let j = 0; j < 10; j++) {
+				x = initialX + i * (w + gap);
+				y = initialY + j * (h + gap);
+				context.beginPath();
+				context.rect(x, y, w, h);
+				context.strokeStyle = "#D4D6B9";
+				context.stroke();
+
+				if (Math.random() < 0.5) {
+					context.beginPath();
+					context.rect(x + offset, y + offset, w - offset * 2, h - offset * 2);
+					context.strokeStyle = "#D4D6B9";
+					context.stroke();
+				}
+			}
+		}
+	};
 };
 
 canvasSketch(sketch, settings);
